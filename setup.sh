7b987e47bf51f9482a4c12306281831353333f65
mjmiller3416 <mj.miller3416@gmail.com>
2025-06-22 14:13:33 -0400

    Updated setup.sh to include pytest dependancy.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,4 @@
 # Only install if not already installed
 pip show PySide6 > /dev/null 2>&1 || pip install PySide6==6.6.1
 pip show pydantic > /dev/null 2>&1 || pip install pydantic==2.6.4
-
-# You can add more safety checks or echo steps if needed
+pip show pytest > /dev/null 2>&1 || pip install pytest==8.2.1
