070ce714d1ab4e4954a2c501967b49b6bf743ee9
mjmiller3416 <mj.miller3416@gmail.com>
2025-06-22 14:42:52 -0400

    Updated setup.sh to include color

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@
 pip show PySide6 > /dev/null 2>&1 || pip install PySide6==6.6.1
 pip show pydantic > /dev/null 2>&1 || pip install pydantic==2.6.4
 pip show pytest > /dev/null 2>&1 || pip install pytest==8.2.1
+pip show colorlog > /dev/null 2>&1 || pip install colorlog
