fc31492a4c12daf948b3fa9752e3717afc9ccb8d
mjmiller3416 <mj.miller3416@gmail.com>
2025-06-23 15:15:06 -0400

    refactor: adjust DialogWindow initialization

--- app/ui/components/dialogs/dialog_window.py
+++ app/ui/components/dialogs/dialog_window.py
@@ -8,133 +8,17 @@ Provides close button and dragging functionality.
 # ── Imports ─────────────────────────────────────────────────────────────────────
 from PySide6.QtCore import QEvent, QPoint, QRect, Qt, Signal
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtWidgets import (QDialog, QHBoxLayout, QLabel, QSizeGrip,
-                               QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (
+    QDialog, QHBoxLayout, QLabel, QSizeGrip, QVBoxLayout, QWidget)
 
 from app.config import APPLICATION_WINDOW
 from app.ui.widgets import CTToolButton
 from app.ui.widgets.helpers import ButtonEffects
+from app.ui.components.navigation.titlebar import TitleBar
 
 # ── Constants ───────────────────────────────────────────────────────────────────
 SETTINGS = APPLICATION_WINDOW["SETTINGS"]
 ICONS = APPLICATION_WINDOW["ICONS"]
-
-# ── Title Bar  ──────────────────────────────────────────────────────────────────
-class TitleBar(QWidget):
-    """A custom title bar widget for frameless dialogs.
-
-    Provides a close button and supports window dragging.
-
-    Attributes:
-        close_clicked: Signal emitted when the close button is clicked.
-        old_pos: Stores the last mouse position during a drag operation.
-        lbl_title: Label displaying the dialog title.
-        btn_ico_close: Button to close the dialog.
-    """
-
-
-    # ── Signals ─────────────────────────────────────────────────────────────────────
-    close_clicked = Signal()  # Signal emitted when close button is clicked
-
-    def __init__(self, parent, window_title: str = SETTINGS["APP_NAME"]):
-        """Initializes the TitleBar.
-
-        Args:
-            parent (QWidget): The parent widget.
-            window_title (str): The title text to display.
-        """
-        # ── Properties ──
-        super().__init__(parent)
-        self.setObjectName("TitleBar") 
-        self.setAttribute(Qt.WA_StyledBackground)
-        self.setFixedHeight(38)
-
-        self.old_pos = None  # initialize old position for dragging
-
-        # ── Title Label ──
-        self.lbl_title = QLabel(window_title, self)
-        
-        # ── Close Button ──
-        self.btn_ico_close = CTToolButton(
-            file_path = ICONS["CLOSE"]["PATH"],
-            icon_size = ICONS["CLOSE"]["SIZE"],
-            variant   = ICONS["CLOSE"]["DYNAMIC"],
-        )
-        self.btn_ico_close.setFixedSize(SETTINGS["BTN_SIZE"])
-        self.btn_ico_close.setObjectName("BtnClose")
-
-        self._build_layout()  # build the layout for the title bar
-        self._connect_signals() # connect button signals
-
-    def _connect_signals(self):
-        """Connects signals from buttons to the TitleBar's signals."""
-        self.btn_ico_close.clicked.connect(self.close_clicked)
-
-    def _build_layout(self):
-        """Builds the layout for the title bar, arranging buttons and title."""
-        title_bar_layout = QHBoxLayout(self)
-        title_bar_layout.addWidget(self.lbl_title)
-        title_bar_layout.addStretch(1)  # push buttons to the right
-        title_bar_layout.addWidget(self.btn_ico_close)
-        title_bar_layout.setContentsMargins(0, 0, 0, 0) 
-
-    @property
-    def buttons(self):
-        """Returns a dictionary of the control buttons.
-
-        Returns:
-            dict: A dictionary mapping button names to their respective
-                  CTToolButton instances.
-        """
-        return {
-            "close": self.btn_ico_close,
-        }
-
-    def mousePressEvent(self, event):
-        """Handles mouse press events to initiate window dragging.
-
-        Args:
-            event (QMouseEvent): The mouse press event.
-        """
-        # Allow dragging of dialog window
-        if event.button() == Qt.LeftButton:
-            self.old_pos = event.globalPos()
-        super().mousePressEvent(event)
-
-    def mouseMoveEvent(self, event):
-        """Handles mouse move events to drag the window.
-
-        Args:
-            event (QMouseEvent): The mouse move event.
-        """
-        if self.old_pos:
-            delta = event.globalPos() - self.old_pos
-            win = self.window()
-            win.move(win.x() + delta.x(), win.y() + delta.y())
-            self.old_pos = event.globalPos()
-        super().mouseMoveEvent(event)
-
-    def mouseReleaseEvent(self, event):
-        """Handles mouse release events to stop dragging.
-
-        Args:
-            event (QMouseEvent): The mouse release event.
-        """
-        self.old_pos = None
-        self.setCursor(Qt.ArrowCursor)
-        super().mouseReleaseEvent(event)
-
-    def mouseDoubleClickEvent(self, event):
-        """Handles a double-click event on the title bar.
-
-        This triggers the maximize/restore functionality.
-
-        Args:
-            event (QMouseEvent): The mouse double-click event.
-        """
-        if event.button() == Qt.LeftButton:
-            self.maximize_clicked.emit()
-        super().mouseDoubleClickEvent(event)
 
 # ── Application Window ──────────────────────────────────────────────────────────
 class DialogWindow(QDialog):
@@ -172,7 +56,6 @@ class DialogWindow(QDialog):
 
         # ── Title Bar ──
         self.title_bar = TitleBar(self)
-        self.title_bar.lbl_title.setText(window_title)
         self.central_layout.addWidget(self.title_bar)
         # Connect title bar signals for dialog behavior
         self.title_bar.close_clicked.connect(self.close)
